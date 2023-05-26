e14b8bc9ae926d2ab3045b5995be8aa2b89e24bc
malou55 <104149213+malou55@users.noreply.github.com>
2023-05-26 14:24:41 +0200

    Add files via upload

---
+++ details.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<title>description detaillée de ramadan 2020</title>
+<body>
+	<article>
+		<h1> Ramadan 2020 </h1>
+	<p>
+durant le mois de Ramadan 2020 nous avons pu récolter un montant d'environ 30000000 dinars Algériens , grace à un don amine benfatah 
+nous avons aidé une diziane de familles dans le besoin en leur fournissant de la nouriteure tout le mois <br>
+les familles bénéficiant de l'aide:
+brandon jacob
+</p>
+</article>
+</body>
